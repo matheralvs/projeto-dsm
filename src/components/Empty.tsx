e2f6dab0113bfea24c5c0085da28a2af2e7eb0ea
Matheus Alves <mtalves27@gmail.com>
2023-07-10 01:23:46 -0300

    chore: created empty component

---
+++ src/components/Empty.tsx
@@ -0,0 +1,19 @@
+import Feather from '@expo/vector-icons/Feather'
+import { Icon, Text, VStack } from 'native-base'
+
+export function Empty() {
+  return (
+    <VStack alignItems="center" px={5} py={12}>
+      <Icon as={Feather} name="inbox" size="6xl" color="gray.500" />
+
+      <VStack alignItems="center">
+        <Text fontSize="md" fontFamily="heading" color="gray.500">
+          Ops! Não temos nada por aqui.
+        </Text>
+        <Text fontSize="md" color="gray.500">
+          Pesquise agora mesmo um livro e se divirta!
+        </Text>
+      </VStack>
+    </VStack>
+  )
+}
