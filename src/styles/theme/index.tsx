c1426de0662781c97e21a7c8864a23a4a4902090
Matheus Alves <mtalves27@gmail.com>
2023-07-10 01:24:52 -0300

    chore: added themes and packages

---
+++ src/styles/theme/index.tsx
@@ -0,0 +1,15 @@
+import { extendTheme } from 'native-base'
+
+export const defaultTheme = extendTheme({
+  fonts: {
+    heading: 'Inter700Bold',
+    body: 'Inter400Regular',
+  },
+  fontSizes: {
+    xs: 12,
+    sm: 14,
+  },
+  sizes: {
+    14: 56,
+  },
+})
