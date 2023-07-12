2d8ce8e3e1c8435df30e5ffc1d0327a05b2c6874
Matheus Alves <mtalves27@gmail.com>
2023-07-12 03:30:10 -0300

    chore: adjust index

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,43 @@
-import { Stack, Text } from 'native-base'
+import { ScrollView, Stack } from 'native-base'
+
+import { Information, Card } from '@components'
 
 export default function Home() {
   return (
-    <Stack px={4}>
-      <Text color="black" fontSize="3xl">
-        Home
-      </Text>
+    <Stack px={4} space={4}>
+      <Information />
+
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        _contentContainerStyle={{
+          paddingBottom: 20,
+        }}
+      >
+        <Card.Root>
+          <Card.Content />
+          <Card.Footer />
+        </Card.Root>
+
+        <Card.Root>
+          <Card.Content />
+          <Card.Footer />
+        </Card.Root>
+
+        <Card.Root>
+          <Card.Content />
+          <Card.Footer />
+        </Card.Root>
+
+        <Card.Root>
+          <Card.Content />
+          <Card.Footer />
+        </Card.Root>
+
+        <Card.Root>
+          <Card.Content />
+          <Card.Footer />
+        </Card.Root>
+      </ScrollView>
     </Stack>
   )
 }
