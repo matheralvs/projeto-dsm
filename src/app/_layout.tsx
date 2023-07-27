51021c3cc313daf3f6b809e45a63eeb5337d82f5
Matheus Alves <mtalves27@gmail.com>
2023-07-27 02:17:35 -0300

    chore: added redux provider in layout

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,6 @@
 import { Slot } from 'expo-router'
 import { StatusBar } from 'expo-status-bar'
+import { Provider as ReduxProvider } from 'react-redux'
 
 import {
   useFonts,
@@ -7,9 +8,9 @@ import {
   Inter_700Bold as Inter700Bold,
 } from '@expo-google-fonts/inter'
 
-import { Box, NativeBaseProvider } from 'native-base'
+import { NativeBaseProvider } from 'native-base'
 
-import { Header, Empty } from '@components'
+import { store } from '@store'
 
 import { defaultTheme } from '@styles/theme'
 
@@ -25,15 +26,10 @@ export default function RootLayout() {
 
   return (
     <NativeBaseProvider theme={defaultTheme}>
-      <StatusBar style="light" translucent />
-      <Header.Root>
-        <Header.Logo />
-        <Header.Search />
-      </Header.Root>
-      <Box flex={1} mt={12}>
-        {/* <Empty /> */}
+      <ReduxProvider store={store}>
+        <StatusBar style="light" translucent />
         <Slot />
-      </Box>
+      </ReduxProvider>
     </NativeBaseProvider>
   )
 }
