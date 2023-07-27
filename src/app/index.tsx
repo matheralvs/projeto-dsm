8793a4c1d93e40814d88d80a349a0d15543210cc
Matheus Alves <mtalves27@gmail.com>
2023-07-27 02:18:00 -0300

    chore: clean the home page

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,43 +1,17 @@
-import { ScrollView, Stack } from 'native-base'
+import { Stack } from 'native-base'
 
-import { Information, Card } from '@components'
+import { Header, Search } from '@components'
+import { CardsList } from '@components/CardsList'
 
 export default function Home() {
   return (
-    <Stack px={4} space={4}>
-      <Information />
+    <Stack>
+      <Header.Root>
+        <Header.Logo />
+        <Search />
+      </Header.Root>
 
-      <ScrollView
-        showsVerticalScrollIndicator={false}
-        _contentContainerStyle={{
-          paddingBottom: 20,
-        }}
-      >
-        <Card.Root>
-          <Card.Content />
-          <Card.Footer />
-        </Card.Root>
-
-        <Card.Root>
-          <Card.Content />
-          <Card.Footer />
-        </Card.Root>
-
-        <Card.Root>
-          <Card.Content />
-          <Card.Footer />
-        </Card.Root>
-
-        <Card.Root>
-          <Card.Content />
-          <Card.Footer />
-        </Card.Root>
-
-        <Card.Root>
-          <Card.Content />
-          <Card.Footer />
-        </Card.Root>
-      </ScrollView>
+      <CardsList />
     </Stack>
   )
 }
